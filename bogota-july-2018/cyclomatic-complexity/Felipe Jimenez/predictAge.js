512dd6d5d70f98a107026f7e99196e66234904f1
FelipeJB <felipejbq@hotmail.com>
2018-07-24 19:58:47 -0500

    Correct naming issues.

--- bogota-july-2018/cyclomatic-complexity/Felipe Jimenez/predictAge.js
+++ bogota-july-2018/cyclomatic-complexity/Felipe Jimenez/predictAge.js
@@ -1,7 +1,8 @@
 function predictAge(age1,age2,age3,age4,age5,age6,age7,age8){
-  	var multipliedAges=[age1*age1,age2*age2,age3*age3,age4*age4,age5*age5,age6*age6,age7*age7,age8*age8];
-	var agesSum=multipliedAges[0]+multipliedAges[1]+multipliedAges[2]+multipliedAges[3]+multipliedAges[4]+multipliedAges[5]+multipliedAges[6]+multipliedAges[7];
-	var ageSquare=Math.sqrt(agesSum);
-	var prediction=ageSquare/2;
-	return floor(prediction);	
+	//Considering there always will be only 8 ages, this approach will always be the one with the least cyclomatic complexity
+	let multipliedAges=[age1*age1,age2*age2,age3*age3,age4*age4,age5*age5,age6*age6,age7*age7,age8*age8];
+	let agesSum=multipliedAges[0]+multipliedAges[1]+multipliedAges[2]+multipliedAges[3]+multipliedAges[4]+multipliedAges[5]+multipliedAges[6]+multipliedAges[7];
+	let ageSquareRoot=Math.sqrt(agesSum);
+	let prediction=ageSquareRoot/2;
+  	return Math.floor(prediction);
 }
