79bf979a2233ed24f95e17f60ef6ce4ced6d5b39
JulianOrjuela <u1202021@unimilitar.edu.co>
2018-07-24 21:05:55 -0500

    Kata_update
    Changes to the kata solution and addition to the image showing the flowchart and cyclomatic complexity of the kata

--- bogota-july-2018/cyclomatic-complexity/Kata_Julian_Orjuela_Pelaez/Kata.js
+++ bogota-july-2018/cyclomatic-complexity/Kata_Julian_Orjuela_Pelaez/Kata.js
@@ -1,19 +1,14 @@
-function numRaisedBy(num, index){
-  index = 2;
-  return Math.pow(num,index);
+function numRaisedBy(num){
+  return num*num;
 }
 
 function numSum(total,actualNumber){
   return total + actualNumber;
 }
 
-function divide(numerator,denominator){
-  return numerator/denominator;
-}
-
 function predictAge(age1,age2,age3,age4,age5,age6,age7,age8){
   const ageArray = [age1,age2,age3,age4,age5,age6,age7,age8];
   let raisedArray = ageArray.map(numRaisedBy);
   let Age = raisedArray.reduce(numSum);
-  return Math.floor(Math.sqrt(Age)/2)
+  return Math.floor(Math.sqrt(Age)/2);
 }
