7b99c403e2f9c5fc186dcc129f88e6bf5add3d17
Cristian Paredes <cristiandparedesc@gmail.com>
2018-07-25 00:29:20 -0500

    Calculator UI
    Calculator UI finished implementing previous js Script.

--- bogota-july-2018/homework-calculator/calculator.js
+++ bogota-july-2018/homework-calculator/calculator.js
@@ -1,6 +1,43 @@
-function myCalculator (...operation)  {
-	const values = operation.join(' ');
-  return result = eval(values);  
+let operation = "";
+
+myCalculator = (...operation) => {
+    const values = operation.join('');
+    let result = eval(values);
+    result = fixNumber(result);
+    return result;
 }
 
-console.log(myCalculator("(10+2)*5","+","15"));
+fixNumber = (number) => {
+    let fixedNumber;
+    if (number % 1 != 0) {
+        fixedNumber = number.toPrecision(7);
+    } else {
+        if (number.toString().length > 7) {
+            fixedNumber = number.toExponential();
+            const base = fixedNumber.split('e+');
+            fixedNumber = Number(base[0]).toFixed(4) + "e+" + base[1];
+        }
+        else {
+            fixedNumber = number;
+        }
+    }
+    return fixedNumber;
+}
+
+addToOperation = val => {
+    operation += val;
+    document.getElementById("operation").getElementsByTagName("P")[0].innerHTML = operation;
+}
+
+getResult = () => {
+    let result = this.myCalculator(operation);
+    document.getElementById("result").getElementsByTagName("P")[0].innerHTML = result;
+    document.getElementById("operation").getElementsByTagName("P")[0].innerHTML = result;
+    operation = result;
+}
+
+clearAll = () => {
+    document.getElementById("operation").getElementsByTagName("P")[0].innerHTML = "";
+    document.getElementById("result").getElementsByTagName("P")[0].innerHTML = "";
+    operation = "";
+}
