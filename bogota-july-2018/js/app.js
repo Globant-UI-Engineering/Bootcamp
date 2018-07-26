5dcb5d122a4471892f1c95c65d5dc6c45a3d7872
Dom <25825490+domgalop@users.noreply.github.com>
2018-07-26 13:50:45 -0500

    Change config to Airbnb style guide

--- bogota-july-2018/js/app.js
+++ bogota-july-2018/js/app.js
@@ -1,134 +1,139 @@
 /* ======= Model ======= */
 
 let model = {
-    number1:0,
-    number2:0
-}
+  number1: 0,
+  number2: 0
+};
 
 /* ======= Controller ======= */
 
-let controller =  {
+const controller = {
+  setNumber(modelNumber, value) {
+    model[modelNumber] = value;
+  },
 
-    init(){
-        view.init();
-    },
+  reset() {
+    model = {
+      number1: 0,
+      number2: 0
+    };
+  },
 
-    setNumber(modelNumber, value){
-        model[modelNumber] = value ; 
-    },
+  getNumber(number) {
+    return model[number];
+  },
 
-    getModel(){
-        return model;
-    },
+  sum(numb1, numb2) {
+    return numb1 + numb2;
+  },
 
-    reset(){
-        model = {
-            number1:0,
-            number2:0
-        }
-    },
+  rest(numb1, numb2) {
+    return numb1 - numb2;
+  },
 
-    getNumber(number){
-       return model[number];
-    },
+  multiplication(numb1, numb2) {
+    return numb1 * numb2;
+  },
 
-    sum(numb1, numb2){
-        return numb1 + numb2;
-    },
+  division(numb1, numb2) {
+    return parseFloat(numb1 / numb2);
+  },
 
-    rest(numb1, numb2){
-        return numb1 - numb2;
-    },
+  percentage(numb1, per) {
+    return parseFloat((numb1 / 100) * per);
+  },
 
-    multiplication(numb1, numb2){
-        return numb1 * numb2;
-    },
-
-    division(numb1, numb2){
-        return parseFloat(numb1 / numb2);
-    },
-
-    percentage(numb1, per){
-        return parseFloat((numb1 /100) * per);
-    },
-    
-    changeSymbol(modelNumber, symbol){
-        let change = symbol ? Math.abs(model[modelNumber]): -Math.abs(model[modelNumber]);
-        model[modelNumber] = change;
-        return change;     
-    }
-}
+  changeSymbol(modelNumber, symbol) {
+    const change = symbol
+      ? Math.abs(model[modelNumber])
+      : -Math.abs(model[modelNumber]);
+    model[modelNumber] = change;
+    return change;
+  }
+};
 
 /* ======= View ======= */
 
-let view = {
-    init(){
-        this.container = document.getElementById("container");
-        this.display = document.getElementById("display");
-        this.result = document.getElementById("result");
-        this.changeValue = document.getElementById("changeValue");
-        this.clear = document.getElementById("clear");
-        this.renderValue();
-    },
+const view = {
+  init() {
+    this.container = document.getElementById("container");
+    this.display = document.getElementById("display");
+    this.result = document.getElementById("result");
+    this.changeValue = document.getElementById("changeValue");
+    this.clear = document.getElementById("clear");
+    this.renderValue();
+  },
 
-    currentOperation(numb1, numb2, typeOfOperation = 0 ){
-        switch(typeOfOperation){
-            case '+':
-                return controller.sum(numb1,numb2);
-            case '-':
-                return controller.rest(numb1,numb2);
-            case '/':
-                return controller.division(numb1,numb2);   
-            case 'x':
-                return controller.multiplication(numb1,numb2);
-            case '%':
-                return controller.percentage(numb1,numb2);
-            case 0:
-                return 0;
-        }
-    },
-    
-    renderValue(){
-        let state = true;
-        let typeOfOperation;
-        let changeSymbol = true;
-        this.display.textContent = controller.getNumber('number1');
-        let newNumber = '';
-        this.container.addEventListener('click',(e) =>{
-            changeSymbol = !changeSymbol;
-            switch(e.target.textContent){
-                case '+':
-                case '-':
-                case 'x':
-                case '/':
-                case '%':
-                    state = !state;
-                    typeOfOperation = e.target.textContent;
-                    newNumber ="";
-                    break;
-                case '=':
-                    this.display.textContent = this.currentOperation(controller.getNumber('number1'),controller.getNumber('number2'),typeOfOperation)
-                    controller.setNumber('number1',parseInt(this.display.textContent));
-                    break;
-                case 'AC':
-                    controller.reset();
-                    state = true;
-                    this.display.textContent = '0';
-                    newNumber = ''
-                    break;
-                case '+/-':
-                    state ? controller.changeSymbol('number1',changeSymbol): controller.changeSymbol('number2',changeSymbol);
-                    state ? this.display.textContent = controller.getNumber('number1'): this.display.textContent = controller.getNumber('number2');
-                    break;
-                default:
-                    newNumber += (e.target.textContent);
-                    state ? controller.setNumber('number1',parseInt(newNumber)):controller.setNumber('number2', parseInt(newNumber));
-                    this.display.textContent = newNumber;
-                    break;
-            }
-        })
+  currentOperation(numb1, numb2, typeOfOperation) {
+    switch (typeOfOperation) {
+      case "+":
+        return controller.sum(numb1, numb2);
+      case "-":
+        return controller.rest(numb1, numb2);
+      case "/":
+        return controller.division(numb1, numb2);
+      case "x":
+        return controller.multiplication(numb1, numb2);
+      case "%":
+        return controller.percentage(numb1, numb2);
+      default:
+        return 0;
     }
-}
+  },
 
-controller.init();
+  renderValue() {
+    let state = true;
+    let typeOfOperation;
+    let changeSymbol = true;
+    this.display.textContent = controller.getNumber("number1");
+    let newNumber = "";
+    this.container.addEventListener("click", e => {
+      changeSymbol = !changeSymbol;
+      switch (e.target.textContent) {
+        case "+":
+        case "-":
+        case "x":
+        case "/":
+        case "%":
+          state = !state;
+          typeOfOperation = e.target.textContent;
+          newNumber = "";
+          break;
+        case "=":
+          this.display.textContent = this.currentOperation(
+            controller.getNumber("number1"),
+            controller.getNumber("number2"),
+            typeOfOperation
+          );
+          controller.setNumber(
+            "number1",
+            parseInt(this.display.textContent, 10)
+          );
+          break;
+        case "AC":
+          controller.reset();
+          state = true;
+          this.display.textContent = "0";
+          newNumber = "";
+          break;
+        case "+/-":
+          state
+            ? controller.changeSymbol("number1", changeSymbol)
+            : controller.changeSymbol("number2", changeSymbol);
+          state
+            ? (this.display.textContent = controller.getNumber("number1"))
+            : (this.display.textContent = controller.getNumber("number2"));
+          break;
+        default:
+          newNumber += e.target.textContent;
+          state
+            ? controller.setNumber("number1", parseInt(newNumber, 10))
+            : controller.setNumber("number2", parseInt(newNumber, 10));
+          this.display.textContent = newNumber;
+          break;
+      }
+    });
+  }
+};
 
+view.init();
