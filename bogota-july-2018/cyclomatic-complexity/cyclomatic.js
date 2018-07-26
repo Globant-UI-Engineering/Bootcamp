f510d8dd89c7b5ab70d045f4a7f9d03be6dd73ec
Juan Forero <juanfopaez@gmail.com>
2018-07-26 07:58:42 -0500

    [BootCamp]Cyclomatic floor problem solved

--- bogota-july-2018/cyclomatic-complexity/cyclomatic.js
+++ bogota-july-2018/cyclomatic-complexity/cyclomatic.js
@@ -1,8 +1,8 @@
-function predictionAge(...ages){
+function predictAge(age0,age2,age3,age4,age5,age6,age7,age8){
     let solution=0;
     for (let i = 0; i <arguments.length ; i++) {
          solution += Math.pow(arguments[i],2);
     }
-    solution=(Math.sqrt(solution))/2;
+    solution=Math.floor((Math.sqrt(solution))/2);
     return solution;
-}
+ }
