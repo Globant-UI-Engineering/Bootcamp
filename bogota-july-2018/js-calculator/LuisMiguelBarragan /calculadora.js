4b2e702738238997efb7aedc856857e02b6139df
Luis Miguel <lucho7030@gmail.com>
2018-07-26 00:10:50 -0500

    Implement UI-Calculator

--- bogota-july-2018/js-calculator/LuisMiguelBarragan /calculadora.js
+++ bogota-july-2018/js-calculator/LuisMiguelBarragan /calculadora.js
@@ -1,3 +1,8 @@
+let data = {
+    "a": "",
+    "b": "",
+    "op": ""
+}
 
 const operators = {
     "add": function (a, b) { return a + b; },
@@ -6,9 +11,49 @@ const operators = {
     "divide": function (a, b) { return a / b; },
 };
 
-function calculator (a, b, op) {
-    const res = operators[op](a, b);
-    console.log(res);
-}
+let nums = document.getElementById('num');
+let op = document.getElementById('operator');
+let equal = document.getElementById('equal');
+let del = document.getElementById('del');
+let print = document.querySelector('.container > div:first-child > span');
 
-this.calculator(10, 20, "divide");
+function calculator (a, b, op) { 
+    if(data.op) {
+        data.a = operators[op](a, b);
+        data.b = "";
+    }
+    print.innerHTML = data.a;
+};
+
+function addNumber(num) {
+    if(data.op) {
+        data.b = data.b.concat(num.toString());
+        print.innerHTML = data.b;
+    }else {
+        data.a = data.a.concat(num.toString());
+        print.innerHTML = data.a;
+    }   
+};
+
+nums.addEventListener("click", number => {
+    let num = number.target.textContent;
+    this.addNumber(num);
+} );
+
+op.addEventListener("click", (e) => {
+    data.op = e.target.value;
+});
+
+equal.addEventListener("click", () => {
+    const a = parseInt(data.a);
+    const b = parseInt(data.b);
+    console.log(a, b);
+    this.calculator(a, b, data.op);
+    data.op = "";
+});
+
+del.addEventListener("click", () => {
+    data.a = "";
+    data.b = "";
+    print.innerHTML = data.a;
+})
