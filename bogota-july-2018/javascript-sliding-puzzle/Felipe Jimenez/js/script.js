33257cec3f9ed398c3a6b8652dec5f0d77728885
FelipeJB <felipejbq@hotmail.com>
2018-07-27 00:06:47 -0500

    Include pieces movement and moves count

--- bogota-july-2018/javascript-sliding-puzzle/Felipe Jimenez/js/script.js
+++ bogota-july-2018/javascript-sliding-puzzle/Felipe Jimenez/js/script.js
@@ -1,6 +1,33 @@
 window.onload= function(){
-	var pieces = createPieces();
-	initiateBoard(pieces);
+	moves=0;
+	globalMovements = getMovements();
+	globalPieces = createPieces();
+	fillBoard();
+}
+
+var globalPieces;
+var globalMovements;
+var moves;
+
+function getMovements(){
+	var movements = new Map();
+	movements.set('0', [1,4]);
+	movements.set('1', [0,2,5]);
+	movements.set('2', [1,3,6]);
+	movements.set('3', [2,7]);
+	movements.set('4', [0,5,8]);
+	movements.set('5', [1,4,6,9]);
+	movements.set('6', [2,5,7,10]);
+	movements.set('7', [3,6,11]);
+	movements.set('8', [4,9,12]);
+	movements.set('9', [5,8,10,13]);
+	movements.set('10', [6,9,11,14]);
+	movements.set('11', [7,10,15]);
+	movements.set('12', [8,13]);
+	movements.set('13', [9,12,14]);
+	movements.set('14', [10,13,15]);
+	movements.set('15', [11,14]);
+	return movements;
 }
 
 function createPieces(){
@@ -18,20 +45,36 @@ function createPieces(){
 	return pieces;
 }
 
-function initiateBoard(pieces){
+function fillBoard(){
 	var field;
 	for(var i=1; i<16; i++){
 		field=document.getElementById('field-' + (i));
-		field.appendChild(pieces[i]);
+		field.appendChild(globalPieces[i]);
  	}
 }
 
 function isFinished(){
-
+	//contenido
 }
 
 function move(piece){
-	console.log("movement");
+	var movesCounter = document.getElementById('moves-counter');
+	var selectedField = piece.parentElement;
+	var selectedFieldIndex = selectedField.getAttribute('id').substring(6);
+	var emptyFieldIndex = globalPieces.indexOf(0);
+	var emptyField = document.getElementById('field-' + (emptyFieldIndex));
+	var emptyFieldMovements = globalMovements.get(emptyFieldIndex.toString());
+
+	if(emptyFieldMovements.includes(parseInt(selectedFieldIndex))){
+		moves++;
+		movesCounter.innerText=("Moves: "+moves);
+		globalPieces[selectedFieldIndex]=0;
+		globalPieces[emptyFieldIndex]=piece;
+		emptyField.appendChild(piece);
+		if(isFinished()){
+			//actualizarMensaje
+		}
+	}
 }
 
 function shuffle(array) {
