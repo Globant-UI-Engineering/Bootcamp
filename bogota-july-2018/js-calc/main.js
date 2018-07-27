50fd11f1196f66e118f4e0fae323facfac3be8e0
ortizjuandavid@gmail.com <ortizjuandavid@gmail.com>
2018-07-27 14:42:14 -0500

    math operations are performed and display result on input tag, include two or more operators are not restricted and if is used will cause error

--- bogota-july-2018/js-calc/main.js
+++ bogota-july-2018/js-calc/main.js
@@ -1,16 +1,61 @@
 window.onload = function (){
     var BtnCalc = document.getElementsByClassName('BtnCalc')
     var inputData =[];
-    console.log(Array.from(BtnCalc));
-    function getValue(){
-        inputData.push(this.value);
+    var indexOfOperator;
+    function buttonHandler(){
+        var keyPressed = getValue(this);
+
+        var controlOperator ={
+            '=': function(){
+                var result = calculator(inputData.toString().replace(/,/g,""));
+                clearInputData();
+                inputData.push(result);
+                printInputData(inputData);
+
+            },
+            'clc': function(){
+                clearInputData();
+                printInputData("");
+            },
+            'x': function(){
+                indexOfOperator=inputData.length;
+                printInputData(keyPressed);
+            },
+            '/': function(){
+                indexOfOperator=inputData.length;
+                printInputData(keyPressed);
+            },
+            '-': function(){
+                indexOfOperator=inputData.length;
+                printInputData(keyPressed);
+            },
+            '+': function(){
+                indexOfOperator=inputData.length;
+                printInputData(keyPressed);
+            },
+        }
+        if(controlOperator[keyPressed]){
+         controlOperator[keyPressed]();
+        }else{
+            printInputData(keyPressed);
+        }
+    }
+    function clearInputData(){
+    inputData.splice(0,inputData.length);
+    }
+    function printInputData(data){
+        if(data){inputData.push(data)};
         document.getElementById('inputMath').value=inputData.toString().replace(/,/g,"");
-        console.log(inputData);
+    }
+    function getValue(btn){
+        return btn.value;
     }
     Array.from(BtnCalc).forEach(function(btn){
-        btn.addEventListener('click',getValue);
+        btn.addEventListener('click',buttonHandler);
     });
     function calculator(operation){
+        var firstNumber =operation.slice(0,indexOfOperator);
+        var secondNumber = operation.slice(indexOfOperator+1);
     var operators ={
         '+' : function(a,b){
             return a+b;
@@ -18,15 +63,15 @@ window.onload = function (){
         '-' : function(a,b){
             return a-b;
         },
-        '*': function(a,b){
+        'x': function(a,b){
             return a*b;
         },
         '/': function(a,b){
             return a/b;
         }
     }
-    if(operators[operation[1]]){
-        return operators[operation[1]](Number(operation[0]),Number(operation[2]));
+    if(operators[operation[indexOfOperator]]){
+        return operators[operation[indexOfOperator]](Number(firstNumber),Number(secondNumber));
     }
     else{
         return null;
