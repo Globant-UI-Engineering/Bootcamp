2da3440c93f1227fdc2835d95d09d799b758220b
Cristian Paredes <cristiandparedesc@gmail.com>
2018-07-27 00:10:33 -0500

    Puzzle Game Finished
    Preliminary version of the game completed.

---
+++ bogota-july-2018/Slide-puzzle-homework/Cristian_Paredes/index.js
@@ -0,0 +1,55 @@
+let emptySlotIndex = {
+    column: '3',
+    row: '3'
+}
+
+let totalMovements = 0;
+
+function getPosition(element) {
+    let column = element.parentNode.className.split("-")[1];
+    let row = element.parentNode.parentNode.className.split("-")[1];
+    moveElement(row, column);
+}
+
+function moveElement(row, column) {
+    if (isMovable(row, column)) {
+        const element = document.getElementsByClassName("row-" + row)[0]
+                                .getElementsByClassName("column-" + column)[0];
+        document.getElementsByClassName("row-" + emptySlotIndex['row'])[0]
+                .getElementsByClassName("column-" + emptySlotIndex['column'])[0].innerHTML = element.innerHTML;
+        element.innerHTML = "";
+        emptySlotIndex['row'] = row;
+        emptySlotIndex['column'] = column;
+        totalMovements++;
+        document.getElementById("total-movements").innerText = totalMovements;
+        this.didUserWon() ? alert("You won!") : null;
+    }
+}
+
+function isMovable(row, column) {
+    if ((row === emptySlotIndex['row'] && Number(column) + 1 == emptySlotIndex['column']) ||
+        (row === emptySlotIndex['row'] && Number(column) - 1 == emptySlotIndex['column'])) {
+        return true;
+    }
+    else if ((column === emptySlotIndex['column'] && Number(row) + 1 == emptySlotIndex['row']) ||
+        (column === emptySlotIndex['column'] && Number(row) - 1 == emptySlotIndex['row'])) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function didUserWon() {
+    let validate = true;
+    for (let i = 1; i <= 15; i++) {
+        if (this.totalMovements !== 0) {
+            var slotKey = document.getElementById("element-"+i).parentNode.getAttribute('id').split("-")[1];
+            if(slotKey != i){
+                validate = false;
+                break;
+            }
+        }
+    }
+    return validate;
+}
