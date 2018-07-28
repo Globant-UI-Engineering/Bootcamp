1941d4f8f663e393581e6d2472db0e56faf9494f
ortizjuandavid@gmail.com <ortizjuandavid@gmail.com>
2018-07-28 13:08:12 -0500

    it's posible to swap any value near to empty box (up/dow or left/right) but the first value selected could not be the empty box

--- bogota-july-2018/javascript-sliding-puzzle/Juan David Ortiz/main.js
+++ bogota-july-2018/javascript-sliding-puzzle/Juan David Ortiz/main.js
@@ -1,10 +1,10 @@
 window.onload = function(){
     var pieces = [["1","2","3","4"],["5","","7","8"],["9","10","11","12"],["13","14","15","6"]];
     var boxes = Array.from(document.getElementsByTagName('div')).slice(2);
+    var idBox;
     function fillBoxes(){
         pieces.forEach(function(row,irow){
             row.forEach(function(item,i){
-                console.log(irow,i,irow*4+i,item);
                 boxes[irow*4+i].innerText=item;
                 boxes[irow*4+i].id=irow+","+i;
             });
@@ -12,24 +12,38 @@ window.onload = function(){
     }
     boxes.forEach(function(box){
         box.addEventListener('click',function(){
-            console.log(findZero(box.id));
+            if(findZero(box.id)){
+                idBox=box.id;
+            }else if(idBox && !box.innerText){
+                swap (idBox,box.id);
+                idBox=undefined;
+            }else{
+                idBox=undefined;
+            }
         })
     });
+    function swap (idBox, idEmptyBox){
+        let idBoxRow = idBox.slice(0,1);
+        let idBoxColumn = idBox.slice(2);
+        let idEmptyBoxRow = idEmptyBox.slice(0,1);
+        let idEmptyBoxColumn = idEmptyBox.slice(2);
+        let idBoxValue = pieces[idBoxRow][idBoxColumn];        
+        pieces[idEmptyBoxRow][idEmptyBoxColumn]= idBoxValue;
+        pieces[idBoxRow][idBoxColumn]= "";
+        fillBoxes();
+    }
     function findZero(id){
         var row=id.slice(0,1);
         var column =id.slice(2);
         var findedZero=false;
         if(pieces[row].indexOf("")>-1){
-            console.log("in this row exist zero",Math.abs(column-pieces[row].indexOf("")));
             findedZero = Math.abs(column-pieces[row].indexOf(""))==1;
         }
         pieces.forEach(function(piecesRow,index){
             if(!piecesRow[column]){
-                console.log("in this column exist zero",Math.abs(row-index));
                 findedZero = Math.abs(row-index)==1;
             }
         });
-//        console.log(row , column, pieces[row][column]);
         return findedZero;
     }
     fillBoxes();
