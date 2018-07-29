f0e7d66360a5e2395ae3a1a8ced63310f161f04a
cristianhdez23 <cristianhernandez.ch878@gmail.com>
2018-07-29 17:28:51 -0500

    Normal functions changed to arrow functions

--- bogota-july-2018/javascript-sliding-puzzle/Cristian_Hernandez/js/puzzle.js
+++ bogota-july-2018/javascript-sliding-puzzle/Cristian_Hernandez/js/puzzle.js
@@ -1,13 +1,12 @@
   const slabElements = document.getElementsByClassName('slab'); // List of elements with class="slab"
   const  slabPlacementOrder = {'Pos-1': null,'Pos-2': null,'Pos-3': null,'Pos-4': null,'Pos-5': null,'Pos-6': null,'Pos-7': null,'Pos-8': null,'Pos-9': null};
   let movableSlabPosition;
-  slabPlacementOrder
   window.onload = function(){
     createNewGame();
     document.getElementsByClassName('board')[0].addEventListener('click', clickedSlab);
   }
 
-  function createNewGame() {
+   createNewGame => {
     let  slabPlacementOrderArray = [7,2,8,5,1,9,3,4,6];
     // Take the array we just shuffled and set the position of each number (from 1 to 9) acording to the order the arrays says. 
      slabPlacementOrderArray.forEach((val, i) => {
@@ -21,7 +20,7 @@
     movableSlabPosition = getAvailablePositions( slabPlacementOrder[9]);  
   }
 
-  function clickedSlab(e) {
+  clickedSlab(e => {
     //Take the value of the slab we clicked.
     let slabValue = e.target.getAttribute('data-slab');
     //Check if the slab we clicked is inside our object 'movableSlabPosition'
@@ -30,9 +29,9 @@
       //if is true, slabs switch places
       newSlabPosition(slabValue);
     }
-  }
+  });
   
-  function newSlabPosition(tileNum) {
+  newSlabPosition(tileNum => {
     //Swap values in  slabPlacementOrder object
     [ slabPlacementOrder[9],  slabPlacementOrder[tileNum]] = [ slabPlacementOrder[tileNum],  slabPlacementOrder[9]]
   
@@ -41,9 +40,9 @@
     slabElements[tileNum - 1].className = `slab slab-order-${ slabPlacementOrder[tileNum]}`; // Slab clicked
   
     movableSlabPosition = getAvailablePositions(slabPlacementOrder[9]);
-  }
+  });
   
-  function getAvailablePositions(emptySlab) {
+  getAvailablePositions(emptySlab => {
     let positionsAvailable = {};
 
     if (emptySlab % 3 !== 1) {  // Check if Empty slab can be moved to the Left
@@ -59,13 +58,13 @@
       positionsAvailable[valuesAvailable(emptySlab - 3)] = 1; // Save the Value of the Slab that can be swap
     }
     return positionsAvailable;
-  }
+  });
   
-  function valuesAvailable(position) {
+ valuesAvailable(position => {
     for (let valueSlabAvailable in  slabPlacementOrder) {
       // Value of Positions available to make a swap
       if ( slabPlacementOrder[valueSlabAvailable] == position) {
         return valueSlabAvailable;
       }
     }
-  }
+  });
