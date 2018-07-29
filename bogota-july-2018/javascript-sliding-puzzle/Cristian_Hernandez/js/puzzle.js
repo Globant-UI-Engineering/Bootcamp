a254ec30de705ac681a54bf7c778148f4721f8a8
cristianhdez23 <cristianhernandez.ch878@gmail.com>
2018-07-29 17:45:35 -0500

    Fixed syntax errors for the arrow functions

--- bogota-july-2018/javascript-sliding-puzzle/Cristian_Hernandez/js/puzzle.js
+++ bogota-july-2018/javascript-sliding-puzzle/Cristian_Hernandez/js/puzzle.js
@@ -1,12 +1,11 @@
   const slabElements = document.getElementsByClassName('slab'); // List of elements with class="slab"
-  const  slabPlacementOrder = {'Pos-1': null,'Pos-2': null,'Pos-3': null,'Pos-4': null,'Pos-5': null,'Pos-6': null,'Pos-7': null,'Pos-8': null,'Pos-9': null};
+  const slabPlacementOrder = {'Pos-1': null,'Pos-2': null,'Pos-3': null,'Pos-4': null,'Pos-5': null,'Pos-6': null,'Pos-7': null,'Pos-8': null,'Pos-9': null};
   let movableSlabPosition;
-  window.onload = function(){
-    createNewGame();
-    document.getElementsByClassName('board')[0].addEventListener('click', clickedSlab);
-  }
+ 
+  
 
-   createNewGame => {
+
+  createNewGame = () => {
     let  slabPlacementOrderArray = [7,2,8,5,1,9,3,4,6];
     // Take the array we just shuffled and set the position of each number (from 1 to 9) acording to the order the arrays says. 
      slabPlacementOrderArray.forEach((val, i) => {
@@ -20,7 +19,7 @@
     movableSlabPosition = getAvailablePositions( slabPlacementOrder[9]);  
   }
 
-  clickedSlab(e => {
+  clickedSlab = (e) => {
     //Take the value of the slab we clicked.
     let slabValue = e.target.getAttribute('data-slab');
     //Check if the slab we clicked is inside our object 'movableSlabPosition'
@@ -29,9 +28,9 @@
       //if is true, slabs switch places
       newSlabPosition(slabValue);
     }
-  });
+  };
   
-  newSlabPosition(slabValue => {
+  newSlabPosition = (slabValue) => {
     //Swap values in  slabPlacementOrder object
     [ slabPlacementOrder[9],  slabPlacementOrder[slabValue]] = [ slabPlacementOrder[slabValue],  slabPlacementOrder[9]]
   
@@ -40,9 +39,9 @@
     slabElements[slabValue - 1].className = `slab slab-order-${ slabPlacementOrder[slabValue]}`; // Slab clicked
   
     movableSlabPosition = getAvailablePositions(slabPlacementOrder[9]);
-  });
+  };
   
-  getAvailablePositions(emptySlab => {
+  getAvailablePositions = (emptySlab) => {
     let positionsAvailable = {};
     if (emptySlab % 3 !== 1) {  // Check if Empty slab can be moved to the Left
       positionsAvailable[valuesAvailable(emptySlab - 1)] = 1 ; // Save the Value of the Slab that can be swap
@@ -57,13 +56,18 @@
       positionsAvailable[valuesAvailable(emptySlab - 3)] = 1; // Save the Value of the Slab that can be swap
     }
     return positionsAvailable;
-  });
+  };
   
- valuesAvailable(position => {
+ valuesAvailable= (position) => {
     for (let valueSlabAvailable in  slabPlacementOrder) {
       // Value of Positions available to make a swap
       if ( slabPlacementOrder[valueSlabAvailable] == position) {
         return valueSlabAvailable;
       }
     }
-  });
+  };
+
+  
+    createNewGame();
+    document.getElementsByClassName('board')[0].addEventListener('click', clickedSlab);
+  
