ec2420b5bfc3483bc085d096580f387b8d29d8f2
cristianhdez23 <cristianhernandez.ch878@gmail.com>
2018-07-29 18:37:42 -0500

    Functionality itsOver was added , Reset Button added , css for button added

--- bogota-july-2018/javascript-sliding-puzzle/Cristian_Hernandez/js/puzzle.js
+++ bogota-july-2018/javascript-sliding-puzzle/Cristian_Hernandez/js/puzzle.js
@@ -1,16 +1,13 @@
   const slabElements = document.getElementsByClassName('slab'); // List of elements with class="slab"
-  const slabPlacementOrder = {'Pos-1': null,'Pos-2': null,'Pos-3': null,'Pos-4': null,'Pos-5': null,'Pos-6': null,'Pos-7': null,'Pos-8': null,'Pos-9': null};
+  const slabPlacementOrder = {'1': null,'2': null,'3': null,'4': null,'5': null,'6': null,'7': null,'8': null,'9': null};
   let movableSlabPosition;
  
-  
-
-
-  createNewGame = () => {
-    let  slabPlacementOrderArray = [7,2,8,5,1,9,3,4,6];
+  createNewGame = () => {        //[1,2,3,4,5,6,7,9,8] [7,2,8,5,1,9,3,4,6]
+    let  slabPlacementOrderArray = [7,2,8,5,1,9,3,4,6]; //Pos
     // Take the array we just shuffled and set the position of each number (from 1 to 9) acording to the order the arrays says. 
      slabPlacementOrderArray.forEach((val, i) => {
     //Since arrays start in 0  we add 1 so our object  slabPlacementOrder place the correct position to each value
-       slabPlacementOrder[i + 1] = val;
+       slabPlacementOrder[i + 1] = val; // Val = Pos in Sliding board
     //Check if we are in the last iteration , if is true we add property slab-blank so we know is the empty slab, if is false we add the corresponding order
       let numberElementPosition = i > 7 ? `${ slabPlacementOrder[9]} slab-blank` :  slabPlacementOrder[i + 1];
       slabElements[i].className = `slab slab-order-${numberElementPosition}`;
@@ -32,13 +29,14 @@
   
   newSlabPosition = (slabValue) => {
     //Swap values in  slabPlacementOrder object
-    [ slabPlacementOrder[9],  slabPlacementOrder[slabValue]] = [ slabPlacementOrder[slabValue],  slabPlacementOrder[9]]
+    [slabPlacementOrder[9], slabPlacementOrder[slabValue]] = [slabPlacementOrder[slabValue], slabPlacementOrder[9]]
   
     //Swap tile className positions
-    slabElements[8].className = `slab slab-order-${ slabPlacementOrder[9]} slab-blank`; // empty Slab
-    slabElements[slabValue - 1].className = `slab slab-order-${ slabPlacementOrder[slabValue]}`; // Slab clicked
-  
-    movableSlabPosition = getAvailablePositions(slabPlacementOrder[9]);
+    slabElements[8].className = `slab slab-order-${slabPlacementOrder[9]} slab-blank`; // empty Slab
+    slabElements[slabValue - 1].className = `slab slab-order-${slabPlacementOrder[slabValue]}`; // Slab clicked
+    
+    (itsOver()) ? alert('You won the game') : movableSlabPosition = getAvailablePositions(slabPlacementOrder[9]);
+    
   };
   
   getAvailablePositions = (emptySlab) => {
@@ -58,7 +56,7 @@
     return positionsAvailable;
   };
   
- valuesAvailable= (position) => {
+  valuesAvailable= (position) => {
     for (let valueSlabAvailable in  slabPlacementOrder) {
       // Value of Positions available to make a swap
       if ( slabPlacementOrder[valueSlabAvailable] == position) {
@@ -67,7 +65,16 @@
     }
   };
 
-  
-    createNewGame();
-    document.getElementsByClassName('board')[0].addEventListener('click', clickedSlab);
+  itsOver = () =>{
+    for(let positionSlab in slabPlacementOrder){
+      if (slabPlacementOrder[positionSlab] != positionSlab) {
+        return false; 
+      }
+    }
+    return true;
+  }
+
+  createNewGame();
+  document.getElementsByClassName('board')[0].addEventListener('click', clickedSlab);
+  document.getElementsByClassName('btn-reset')[0].addEventListener('click', createNewGame);
   
