5fc24d7cae6e12bc7578a4cc4dfc743c4b0c3e63
Dom <25825490+domgalop@users.noreply.github.com>
2018-07-30 01:09:32 -0500

    small fixes js sliding

--- bogota-july-2018/Dominik Ortiz Galop/js/app.js
+++ bogota-july-2018/Dominik Ortiz Galop/js/app.js
@@ -1,209 +1,202 @@
-  const model = {
-    sliding: []
-  };
+const model = {
+  sliding: []
+};
+
+const controller = {
   
-  const controller = {
-    addMatrix(matrix) {
-      model.sliding.push(matrix);
-    },
-
-    createNewMatrix(row, column){
-      let counter = 0;
-      let matrix = new Array(row);
-      for (let i = 0, x = matrix.length; i < x; i++) {
-        matrix[i] = new Array(column);
-        for (let j = 0, z = matrix[i].length; j < z; j++) {
-          counter ++;
-          counter === row * column
-            ? matrix[i][j] = 0
-            : matrix[i][j] = counter;
-        }
+  createNewMatrix(row, column) {
+    let counter = 0;
+    const matrix = new Array(row);
+    for (let i = 0, x = matrix.length; i < x; i += 1) {
+      matrix[i] = new Array(column);
+      for (let j = 0, z = matrix[i].length; j < z; j += 1) {
+        counter += 1;
+        counter === row * column
+        ? (matrix[i][j] = 0)
+        : (matrix[i][j] = counter);
       }
-      return matrix;
-    },
-
-    zeroLocation(){
-      let zeroLocation = {
-        x:0,
-        y:0
-      };
-      let matrix = controller.getMatrix();
-      for (let i = 0, x = matrix.length; i < x; i++){
-        for (let j = 0, y = matrix[i].length; j < y; j++){
-          if(matrix[i][j] === 0){
-            zeroLocation.y = j;
-            zeroLocation.x = i;
-          }
-        }
-      }
-      model.sliding[0].zeroLocation = zeroLocation;
-      return model.sliding[0].zeroLocation;
-    },
-
-    createNewGame(row, column) {
-      class Matrix {
-        constructor(row, column) {
-          this.moves = 0,
-          this.matrix = controller.createNewMatrix(row, column),
-          this.zeroLocation = 0
-        }
-      }
-      return new Matrix(row,column);
-    },
-
-    getMatrix(){
-      return model.sliding[0].matrix;
-    },
-
-    getMoves(){
-      return model.sliding[0].moves;
-    },
-
-    changePosition(clickedElement){
-      let zeroPosition = controller.zeroLocation();
-      let elemt = model.sliding[0].matrix[clickedElement.x][clickedElement.y];
-      if( clickedElement.x  === zeroPosition.x){
-        if(clickedElement.y -1 === zeroPosition.y || clickedElement.y + 1 === zeroPosition.y){
-          model.sliding[0].matrix[clickedElement.x][clickedElement.y] = 0;
-          model.sliding[0].matrix[zeroPosition.x][zeroPosition.y] = elemt;
-          controller.addMoves();
-        }
-      }else if(clickedElement.y  === zeroPosition.y){
-        if(clickedElement.x -1 === zeroPosition.x || clickedElement.x + 1 === zeroPosition.x){
-          model.sliding[0].matrix[clickedElement.x][clickedElement.y] = 0;
-          model.sliding[0].matrix[zeroPosition.x][zeroPosition.y] = elemt;
-          controller.addMoves();
-        }
-      }
-    },
-
-    shuffleMatrix(){
-      let originalMatrix = controller.getMatrix()
-      for (let i = originalMatrix.length - 1; i > 0; i--){
-        for(let j = originalMatrix[i].length - 1; i > 0; i--){
-          const m = Math.floor(Math.random() * ( i + 1 ))
-          const n = Math.floor(Math.random() * ( i + 1 ))
-          let temp = originalMatrix[i][j];
-          originalMatrix[i][j] = originalMatrix[m][n];
-          originalMatrix[m][n] = temp;
-        }
-      }
-      this.resetScore();
-      model.sliding[0].matrix = originalMatrix;
-    },
-
-    changeMatrix(row,column){
-      let newMatrix = controller.createNewGame(row,column)
-      return model.sliding[0].matrix = newMatrix.matrix; 
-    },
-
-    addMoves(){
-      model.sliding[0].moves ++;
-    },
-
-    resetScore(){
-      model.sliding[0].moves = 0;
     }
-  };
+    return matrix;
+  },
   
-  const view = {
-    init() {
-      this.container = document.getElementById('container');
-      this.options = document.getElementById('options');
-      this.score = document.getElementById('score');
-      this.RenderMatrix = this.newMatrix();
-      this.renderButtons();
-      this.renderScore();
-      this.container.addEventListener('click', e => this.currentItem(e));
-      this.options.addEventListener('click', e => this.chooseOption(e));
-    },
-
-    newMatrix(row = 5,column =5 ){
-      let newMatrix;
-      controller.addMatrix(controller.createNewGame(row, column));
-      controller.shuffleMatrix();
-      return newMatrix = controller.getMatrix();
-    },
-
-    currentItem(e){
-      let position = {
-        x: parseInt(e.target.dataset.x),
-        y: parseInt(e.target.dataset.y)
-      }
-      controller.changePosition(position)
-      this.renderScore();
-      this.renderButtons();
-    },
-
-    chooseOption(e){
-      switch(e.target.id){
-        case 'shuffle':
-          this.shuffleMatrix();
-          this.renderScore();
-          break;
-        case 'matrix-select':
-          this.changeMatrix(e.target.value);
-          break;
-      }
-    },
-
-    changeMatrix(e){
-      let matrixSize = parseInt(e);
-      switch(e){
-        case'3':
-          this.RenderMatrix = controller.changeMatrix(matrixSize,matrixSize);
-          controller.shuffleMatrix();
-          this.renderButtons();
-          break;
-        case'4':
-          this.RenderMatrix = controller.changeMatrix(matrixSize,matrixSize);
-          controller.shuffleMatrix();
-          this.renderButtons();
-          break;
-        case'5':
-          this.RenderMatrix = controller.changeMatrix(matrixSize,matrixSize);
-          controller.shuffleMatrix();
-          this.renderButtons();
-          break;
-        case'6':
-          this.RenderMatrix = controller.changeMatrix(matrixSize,matrixSize);
-          controller.shuffleMatrix();
-          this.renderButtons();
-          break;
-        case'7':
-          this.RenderMatrix = controller.changeMatrix(matrixSize,matrixSize);
-          controller.shuffleMatrix();
-          this.renderButtons();
-          break;
-      }
-    },
-
-    shuffleMatrix(){
-      controller.shuffleMatrix();
-      this.renderButtons();
-    },
-
-    renderButtons(){
-      this.button = '';
-      for(let i = 0, x = this.RenderMatrix.length; i < x; i++){
-        for (let j = 0, z = this.RenderMatrix[j].length; j < z; j++){
-          if(this.RenderMatrix[i][j]){
-            this.button +=
-            `<button class="btn" data-x="${i}" data-y="${j}">${this.RenderMatrix[i][j]}</button>`
-          }else
-          this.button += `
-          <button class="btn zero" data-x="${i}" data-y="${j}">${this.RenderMatrix[i][j]}</button>
-          `
+  zeroLocation() {
+    const zeroLocation = {
+      x: 0,
+      y: 0
+    };
+    const matrix = controller.getMatrix();
+    for (let i = 0, x = matrix.length; i < x; i += 1) {
+      for (let j = 0, y = matrix[i].length; j < y; j += 1) {
+        if (matrix[i][j] === 0) {
+          zeroLocation.y = j;
+          zeroLocation.x = i;
         }
       }
-      this.container.innerHTML = this.button;
-    },
-
-    renderScore(){
-      this.score.textContent = `Moves: ${controller.getMoves()}`;
     }
+    model.sliding[0].zeroLocation = zeroLocation;
+  },
+  
+  createNewGame(row, column) {
+    class Matrix {
+      constructor(row, column) {
+        this.moves = 0;
+        this.matrix = controller.createNewMatrix(row, column);
+        this.zeroLocation = 0;
+      }
+    }
+    return new Matrix(row, column);
+  },
+  
+  changePosition(clickedElement) {
+    const zeroPosition = model.sliding[0].zeroLocation;
+    const elemt = model.sliding[0].matrix[clickedElement.x][clickedElement.y];
+    if (clickedElement.x === zeroPosition.x) {
+      if (
+        clickedElement.y - 1 === zeroPosition.y ||
+        clickedElement.y + 1 === zeroPosition.y
+      ) {
+        model.sliding[0].matrix[clickedElement.x][clickedElement.y] = 0;
+        model.sliding[0].matrix[zeroPosition.x][zeroPosition.y] = elemt;
+        model.sliding[0].zeroLocation.x = clickedElement.x;
+        model.sliding[0].zeroLocation.y = clickedElement.y;
+        controller.addMoves();
+      }
+    } else if (clickedElement.y === zeroPosition.y) {
+      if (
+        clickedElement.x - 1 === zeroPosition.x ||
+        clickedElement.x + 1 === zeroPosition.x
+      ) {
+        model.sliding[0].matrix[clickedElement.x][clickedElement.y] = 0;
+        model.sliding[0].matrix[zeroPosition.x][zeroPosition.y] = elemt;
+        model.sliding[0].zeroLocation.x = clickedElement.x;
+        model.sliding[0].zeroLocation.y = clickedElement.y;
+        controller.addMoves();
+      }
+    }
+  },
+  
+  shuffleMatrix() {
+    const matrix = model.sliding[0].matrix;
+    for (let i = matrix.length - 1; i > 0; i -= 1) {
+      for (let j = matrix[i].length - 1; i > 0; i -= 1) {
+        const m = Math.floor(Math.random() * (i + 1));
+        const n = Math.floor(Math.random() * (i + 1));
+        const temp = matrix[i][j];
+        matrix[i][j] = matrix[m][n];
+        matrix[m][n] = temp;
+      }
+    }
+    this.resetScore();
+  },
+  
+  changeMatrix(row, column) {
+    const newMatrix = controller.createNewGame(row, column);
+    model.sliding[0].matrix = newMatrix.matrix;
+    return newMatrix.matrix;
+  },
+  
+  addGame(game) {
+    model.sliding.push(game);
+  },
+  
+  addMoves() {
+    model.sliding[0].moves += 1;
+  },
+  
+  resetScore() {
+    model.sliding[0].moves = 0;
+  },
+  
+  getMatrix() {
+    return model.sliding[0].matrix;
+  },
+  
+  getMoves() {
+    return model.sliding[0].moves;
+  }
+};
 
-  };
+const view = {
+  init() {
+    this.container = document.getElementById("container");
+    this.options = document.getElementById("options");
+    this.score = document.getElementById("score");
+    this.renderMatrix = this.newGame();
+    this.size;
+    this.renderButtons(this.renderMatrix);
+    this.renderScore();
+    this.container.addEventListener("click", e => this.currentItem(e));
+    this.options.addEventListener("click", e => this.chooseOption(e));
+  },
   
-  view.init();
+  newGame(row = 5, column = 5) {
+    const game = controller.createNewGame(row, column);
+    controller.addGame(game);
+    controller.shuffleMatrix();
+    const newMatrix = controller.getMatrix();
+    return newMatrix;
+  },
   
+  currentItem(e) {
+    const position = {
+      x: parseInt(e.target.dataset.x, 10),
+      y: parseInt(e.target.dataset.y, 10)
+    };
+    controller.zeroLocation();
+    controller.changePosition(position);
+    this.renderScore();
+    let currentMatrix = controller.getMatrix()
+    this.renderButtons(currentMatrix);
+  },
+
+  chooseOption(e) {
+    this.size = this.container.className.slice(-5);
+    switch (e.target.id) {
+      case "shuffle":
+        const shuffledMatrix = controller.shuffleMatrix()
+        this.renderButtons(shuffledMatrix);
+        this.renderScore();
+        break;
+      case "matrix-select":
+        this.container.classList.add(`size${e.target.value}`);
+        this.changeMatrix(e.target.value, this.size);
+        break;
+      default:
+        break;
+    }
+  },
+  
+  changeMatrix(e, size ) {
+    const matrixSize = parseInt(e, 10);
+    this.container.classList.replace(size, `size${e}`);
+    this.renderMatrix = controller.changeMatrix(matrixSize, matrixSize);
+    controller.resetScore();
+    this.renderScore();
+    this.renderButtons(this.renderMatrix);
+  },
+
+  renderButtons(matrix) {
+    this.button = "";
+    for (let i = 0, x = matrix.length; i < x; i += 1) {
+      for (let j = 0, z = matrix[j].length; j < z; j += 1) {
+        if (matrix[i][j]) {
+          this.button += `<button class="btn" data-x="${i}" data-y="${j}">${
+            matrix[i][j]
+          }</button>`;
+        } else
+        this.button += `
+        <button class="btn zero" data-x="${i}" data-y="${j}">${
+          matrix[i][j]
+        }</button>
+        `;
+      }
+    }
+    this.container.innerHTML = this.button;
+  },
+  
+  renderScore() {
+    this.score.textContent = `Moves: ${controller.getMoves()}`;
+  }
+};
+
+view.init();
