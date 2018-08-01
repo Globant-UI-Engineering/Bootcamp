27a4bd40753822b3c771f81af3ce928e9a0d2450
Juan Forero <juanfopaez@gmail.com>
2018-08-01 11:43:10 -0500

    [BootCamp] Final version Calculator JS Please enter the commit message for your changes. Lines starting

--- bogota-july-2018/javascript-calculator/JuanForero/main.js
+++ bogota-july-2018/javascript-calculator/JuanForero/main.js
@@ -1,57 +1,55 @@
-//save user input
+
 let inputArray=[];
-let state=true;
-let hasDec=false;
 let inputArray2=[];
+let stateInput=true;
+let stateOperation=false;
 let operators=['*','/','+','-','%'];
-//Function for operators
 function setOperation(op){
     if(operators.includes(inputArray[inputArray.length-1]) || inputArray.length==0){
         //do nothing
         }
     else{
-        hasDec=false;
-        state=false;
+        stateOperation=false;
+        stateInput=false;
         inputArray.push(op);
-        document.getElementById('showbox').innerHTML=(inputArray.concat(inputArray2).join('')); 
+        write();
         }
 }
-//Function to setting buttons
-function setText(input){
-    if(state==true){ 
-        
+
+function setNumber(input){
+    if(stateInput==true){ 
     inputArray.push(input);
-    document.getElementById('showbox').innerHTML=(inputArray.concat(inputArray2).join(''));  
+    write();  
      }
      else{
           inputArray2.push(input);
-          document.getElementById('showbox').innerHTML=(inputArray.concat(inputArray2).join('')); 
+          write();
      }
 }
-//Function to decimal
+
 function setDecimal(){
-        if(hasDec==false && state==true){
-         hasDec=true;
-         inputArray.push('.');
-         document.getElementById('showbox').innerHTML=(inputArray.concat(inputArray2).join(''));   
+        if(stateOperation==false && stateInput==true){
+            stateOperation=true;
+            inputArray.push('.');
+            write();  
        }
-       if(hasDec==false && state==false){
-        hasDec=true;
+       if(stateOperation==false && stateInput==false){
+        stateOperation=true;
         inputArray2.push('.');
-        document.getElementById('showbox').innerHTML=(inputArray.concat(inputArray2).join(''));     
+        write();   
       }
     }
-//Function clearscreen
+
+
 function clearScreen(){
     document.getElementById('showbox').innerHTML="0"; 
     inputArray=[];
     inputArray2=[];
-    state=true;
-    hasDec=false; 
+    stateInput=true;
+    stateOperation=false; 
 }
-//Function equal
+
 function equal(){
- 
     let total;
     let inherit;
     if(inputArray.length>0 && inputArray2.length>0){  
@@ -60,26 +58,31 @@ function equal(){
       inherit=total.toString();
       inputArray=(inherit.split(''));
       inputArray2=[];
-      state=true;
-      hasDec=false; 
+      stateInput=true;
+      stateOperation=false; 
     }
 }
-//Delete date
+
+
 function deleteDate(){
-    if(state==false){
+    if(stateInput==false){
      inputArray2.pop();
-     document.getElementById('showbox').innerHTML=(inputArray.concat(inputArray2)).join(''); 
+     write();
      if(inputArray2.length==0){
-     state=true;
+    stateInput=true;
      }   
     }
     else{
     inputArray.pop();
-    document.getElementById('showbox').innerHTML=(inputArray.concat(inputArray2)).join('');  
+    write();
     if(inputArray.length==0){
         document.getElementById('showbox').innerHTML="0"; 
         } 
 }
+}
+
+function write(){
+    document.getElementById('showbox').innerHTML=(inputArray.concat(inputArray2)).join('');  
 }
 
 
