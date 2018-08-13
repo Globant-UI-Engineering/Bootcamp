ac1d5541ccb8298f7a446ba5bf9e80232e8ea117
Andres Valderrama <andres.valderrama@tambourine.com>
2018-08-12 22:23:28 -0500

    basic react todo app

--- bogota-july-2018/todo/andresvalderrama/src/index.js
+++ bogota-july-2018/todo/andresvalderrama/src/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import { render } from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './App'
+
+render(
+  <App />,
+  document.getElementById('root')
+)
