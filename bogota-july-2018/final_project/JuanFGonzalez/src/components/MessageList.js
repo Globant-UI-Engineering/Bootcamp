901651316e2dfe638c166b02b7ef8577908ccef2
Juan F Gonzalez <juan.frankg7@gmail.com>
2018-08-18 01:03:45 -0500

    added scroll functionality

--- bogota-july-2018/final_project/JuanFGonzalez/src/components/MessageList.js
+++ bogota-july-2018/final_project/JuanFGonzalez/src/components/MessageList.js
@@ -1,18 +1,35 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import Message from "./Message";
 
 class MessageList extends React.Component {
+  componentWillUpdate() {
+    const node = ReactDOM.findDOMNode(this);
+    this.shouldScrollToBottom =
+      node.scrollTop + node.clientHeight + 100 >= node.scrollHeight;
+  }
+
+  componentDidUpdate() {
+    if (this.shouldScrollToBottom) {
+      const node = ReactDOM.findDOMNode(this);
+      node.scrollTop = node.scrollHeight;
+    }
+  }
+
   render() {
     return (
-        <div className="message-list">
-            {this.props.messages.map((message, index) => {
-                return (
-                  <Message key={index} username={message.senderId} text={message.text} />
-                )
-            })}
-        </div>
-    )
+      <div className="message-list">
+        {this.props.messages.map((message, index) => {
+          return (
+            <Message
+              key={index}
+              username={message.senderId}
+              text={message.text}
+            />
+          );
+        })}
+      </div>
+    );
   }
 }
-
 export default MessageList;
