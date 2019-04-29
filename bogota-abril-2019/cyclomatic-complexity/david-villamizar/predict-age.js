d69548d224bc2da49cf791c44248d74e1e6e36a7
David Villamizar <davl3232@users.noreply.github.com>
2019-04-29 17:03:16 -0500

    Updated cyclomatic complexity homework

--- bogota-abril-2019/cyclomatic-complexity/david-villamizar/predict-age.js
+++ bogota-abril-2019/cyclomatic-complexity/david-villamizar/predict-age.js
@@ -1,12 +1,6 @@
-function predictAge(age1, age2, age3, age4, age5, age6, age7, age8) {
-  const sum =
-    age1 * age1 +
-    age2 * age2 +
-    age3 * age3 +
-    age4 * age4 +
-    age5 * age5 +
-    age6 * age6 +
-    age7 * age7 +
-    age8 * age8;
-  return Math.floor(Math.sqrt(sum) / 2.0);
+function predictAge(age1,age2,age3,age4,age5,age6,age7,age8){
+  const sum = Array.prototype.slice.call(arguments)
+    .map(age => age * age)
+    .reduce((sum, ageSq) => sum + ageSq);
+  return Math.floor(Math.sqrt(sum)/2.0);
 }
