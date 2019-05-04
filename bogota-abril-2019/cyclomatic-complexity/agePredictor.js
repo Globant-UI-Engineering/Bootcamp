8c5736b91e0e53c24f8ceb52d287b22f058fdf2c
Camilo Andres Moreno <camolave12@gmail.com>
2019-05-04 15:33:35 -0500

    COMMIT: Changes in the age prediction algorithm for better use of arguments.

--- bogota-abril-2019/cyclomatic-complexity/agePredictor.js
+++ bogota-abril-2019/cyclomatic-complexity/agePredictor.js
@@ -1,12 +1,9 @@
-
-
 //la complejidad del algoritmo es 2, se puede encontrar adjunta en el archivo cyclomatic complexity.jpg
-function predictAge(age1, age2, age3, age4, age5, age6, age7, age8) {
-    var ages = [...arguments];
+function predictAge(...arguments) {
     var prediction = 0;
 
-    for (let i = 0; i < ages.length; i++) {
-        prediction += ages[i] * ages[i];
+    for (let i = 0; i < arguments.length; i++) {
+        prediction += arguments[i] * arguments[i];
     }
 
     prediction = Math.floor(Math.sqrt(prediction) / 2);
