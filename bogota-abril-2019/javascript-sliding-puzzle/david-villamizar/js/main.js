49dca6b09880b27e9b9a1cbff036a0412c86bf65
David Villamizar <davl3232@gmail.com>
2019-05-05 11:53:39 -0500

    Added randomize button.

--- bogota-abril-2019/javascript-sliding-puzzle/david-villamizar/js/main.js
+++ bogota-abril-2019/javascript-sliding-puzzle/david-villamizar/js/main.js
@@ -3,6 +3,11 @@ let TILES = [...document.querySelectorAll("#board div")];
 
 const EMPTY_TILE = document.querySelector("#empty-tile");
 
+addListeners(...document.querySelectorAll("#board div.adjacent"));
+document
+  .querySelector("#randomize-button")
+  .addEventListener("click", e => randomizeBoard(16));
+
 function addListeners(...elements) {
   elements.forEach(el => el.addEventListener("click", onClick));
 }
@@ -10,10 +15,8 @@ function removeListeners(...elements) {
   elements.forEach(el => el.removeEventListener("click", onClick));
 }
 
-addListeners(...document.querySelectorAll("#board div.adjacent"));
-
 /**
- * Swaps a tile to the empty space and returns an array with the new adjacent tiles.
+ * Swaps a tile to the empty space and returns the new index of the empty tile.
  * @param {HTMLElement} tile
  * @returns {HTMLElement[]}
  */
@@ -39,7 +42,7 @@ async function swap(tile) {
       ...TILES.slice(tileInd + 1, TILES.length),
     ];
   }
-  console.log("TILES:", TILES);
+
   const placeholderEmpty = document.createElement("div");
   const placeholderTile = document.createElement("div");
   BOARD.replaceChild(placeholderEmpty, EMPTY_TILE);
@@ -47,7 +50,7 @@ async function swap(tile) {
   BOARD.replaceChild(tile, placeholderEmpty);
   BOARD.replaceChild(EMPTY_TILE, placeholderTile);
 
-  return calculateNewAdjacent(tileInd);
+  return tileInd;
 }
 
 function calculateNewAdjacent(emptyTileInd) {
@@ -87,33 +90,47 @@ function calculateNewAdjacent(emptyTileInd) {
   return [upTile, downTile, leftTile, rightTile].filter(tile => !!tile);
 }
 
-async function onClick(e) {
+async function makeMove(tile) {
   const oldAdjacent = [...document.querySelectorAll("#board div.adjacent")];
   removeListeners(...oldAdjacent);
   oldAdjacent.forEach(tile =>
     tile.classList.remove("down", "up", "left", "right", "adjacent"),
   );
 
-  const newAdjacent = await swap(e.target);
+  const newAdjacent = calculateNewAdjacent(await swap(tile));
   addListeners(...newAdjacent);
+  return newAdjacent;
+}
+
+function onClick(e) {
+  makeMove(e.target);
 }
 
 function translateTowards(element, dstElement) {
   const x = dstElement.offsetLeft - element.offsetLeft;
   const y = dstElement.offsetTop - element.offsetTop;
   return new Promise(resolve => {
-    element.addEventListener(
-      "transitionend",
-      e => {
-        elem.removeEventListener("transitionend", onAnimationComplete);
-        resolve(e);
-      },
-      false,
-    );
+    const onAnimationComplete = e => {
+      element.removeEventListener("transitionend", onAnimationComplete);
+      resolve();
+    };
+    element.addEventListener("transitionend", onAnimationComplete, false);
 
     element.style.transform = `translate(${x}px, ${y}px)`;
   });
 }
 
-// TODO: Desordenar.
+/**
+ * Randomize the board by a given number of moves from the initial sorted state.
+ * @param {number} moves
+ */
+async function randomizeBoard(moves) {
+  let adjacent = [...document.querySelectorAll("#board div.adjacent")];
+  for (let i = 0; i < moves; ++i) {
+    adjacent = await makeMove(
+      adjacent[Math.floor(Math.random() * adjacent.length)],
+    );
+  }
+}
+
 // TODO: Detectar cuando gana.
