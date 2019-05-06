c25cb16ebc91f8501515e163424a4a683ce39704
Juan Acero <dev.acerojuan@gmail.com>
2019-05-05 19:26:27 -0500

    a11y form & cyclomatic complexity done

--- bogota-abril-2019/cyclomatic-complexity/Juan Acero/kata - predict your age.js
+++ bogota-abril-2019/cyclomatic-complexity/Juan Acero/kata - predict your age.js
@@ -1,8 +1,6 @@
 function predictAge (a1, a2, a3, a4, a5, a6, a7, a8) {
     var ages = [a1, a2, a3, a4, a5, a6, a7, a8];
-    ages = ages.map(nage => nage * nage);
-    
+    ages = ages.map(nage => nage * nage)
     return (Math.floor(Math.sqrt(ages.reduce((a, b) => a += b)) / 2));
 }
-
 predictAge(65, 60, 75, 55, 60, 63, 64, 45);
