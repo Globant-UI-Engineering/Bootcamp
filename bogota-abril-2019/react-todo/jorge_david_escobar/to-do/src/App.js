24a2949faea1e75ed82cd2cff7f4e2876a37a032
david <jescobar573@misena.edu.co>
2019-05-07 16:16:52 -0500

    pushing html form

---
+++ bogota-abril-2019/react-todo/jorge_david_escobar/to-do/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+//components
+import Header from './header/Header';
+import Content from './content/Content';
+
+function App() {
+  return (
+    <div className="App">
+      
+     <Header/>
+     <Content/>
+    </div>
+  );
+}
+
+
+
+export default App;
---
+++ bogota-abril-2019/react-todo/jorge_david_escobar/to-do/src/content/Content.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './content.css';
+
+function Content() {
+  return (
+    <div className="content">
+        hola
+    </div>
+  );
+}
+
+
+
+export default Content;
