52e03853573d4f690cce51c27eaf257e2703e49d
Marvin <mardancely@gmail.com>
2019-05-09 10:36:02 -0500

    puzzle homework removed, it doesn't belong here

--- bogota-abril-2019/javascript-sliding-puzzle/marvin-cely/js/sliding-puzzle.js
+++
@@ -1,7 +0,0 @@
-let buttonGroup = document.querySelectorAll("button");
-
-Array.from(buttonGroup).forEach(button => {
-    button.addEventListener('click', (event) => {
-        console.log(event.target.parentNode.parentNode.cellIndex, event.target.parentNode.parentNode.parentNode.rowIndex);
-    });
-});
