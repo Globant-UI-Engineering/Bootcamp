6d3a4091db4647bcd0944f91707fbe76e8c343e6
david <jescobar573@misena.edu.co>
2019-05-09 22:32:51 -0500

    'state' is not define

--- bogota-abril-2019/react-todo/jorge_david_escobar/to-do/src/App.js
+++ bogota-abril-2019/react-todo/jorge_david_escobar/to-do/src/App.js
@@ -2,19 +2,34 @@ import React from 'react';
 import './App.css';
 
 //components
-import Header from './header/Header';
-import Content from './content/Content';
+import Header from './components/header/Header';
+import CreateTask from './components/createTask/CreateTask';
+import TodoList from './components/todoList/TodoList';
 
 function App() {
+
+  state = {
+    items: [],
+    id: 0,
+    item: '',
+    editItem: false
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      item: e.target.value
+    });
+  }
+
   return (
     <div className="App">
-      
-     <Header/>
-     <Content/>
+      <Header />
+      <CreateTask item={this.state.item} handleChange={this.handleChange} />
+      <TodoList />
     </div>
   );
 }
-
-
-
-export default App;
+    
+    
+    
+    export default App;
