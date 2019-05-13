5f82a58fed08f4d0f8d26be1abf21dd5809234a2
Juan Chaparro <juanch@outlook.com>
2019-05-13 13:40:11 -0500

    Delete test lines

--- bogota-abril-2019/react-todo/JuanChaparro/src/App.js
+++ bogota-abril-2019/react-todo/JuanChaparro/src/App.js
@@ -4,32 +4,29 @@ import { TODO_URL } from "./constants/connection";
 import "./App.css";
 
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-        	data: []
-        }
-    }
-
-    render() {
-		if (this.state.data.length === 0){
-			console.log("vaci");
+	constructor() {
+		super();
+		this.state = {
+			data: []
 		}
-    	return (
-        	<div className="app">
-            	<h1>My TODO List</h1>
-            	{this.state.data.length !== 0 ? <TodoList data={this.state.data}></TodoList> : <h2>Cargando...</h2>}
-        	</div>
-    	);
 	}
-	
+
+	render() {
+		return (
+			<div className="app">
+				<h1>My TODO List</h1>
+				{this.state.data.length !== 0 ? <TodoList data={this.state.data}></TodoList> : <h2>Cargando...</h2>}
+			</div>
+		);
+	}
+
 	componentDidMount() {
 		fetch(TODO_URL).then((response) => response.json()).then((dataResponse) => {
 			this.setState(
-				{data: dataResponse.todoList}
+				{ data: dataResponse.todoList }
 			)
 		});
-    }
+	}
 }
 
 export default App;
