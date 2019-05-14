d1a99d1bd0e4291bb51162395e47318384a1baf1
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-05-14 18:04:55 -0500

    Destructuring the todos coming from App, and showing them depending if length is empty or not

--- bogota-abril-2019/redux-sample/juan-reyes/todoapp-redux/src/Todos.js
+++ bogota-abril-2019/redux-sample/juan-reyes/todoapp-redux/src/Todos.js
@@ -1,5 +1,23 @@
 import React from 'react';
 
-const Todos = () => {
+const Todos = ({ todos }) => {
 
+  const todoList = todos.length ? (
+    todos.map(todo => {
+      return (
+        <div className="collection-item" key={todo.id}>
+          <span>{todo.task}</span>
+        </div>
+      )
+    })
+  ) : (
+      <p className="center">No to-do 's left</p>
+    )
+  return (
+    <div className="todos collection">
+      {todoList}
+    </div>
+  )
 }
+
+export default Todos;
