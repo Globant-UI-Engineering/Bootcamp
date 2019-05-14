f09b640dbfb053808d7bf3b6511a40d81aab3b6c
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-05-14 18:11:33 -0500

    Passing the function via props, then using the event onClick to select the id and logging it in the console

--- bogota-abril-2019/redux-sample/juan-reyes/todoapp-redux/src/Todos.js
+++ bogota-abril-2019/redux-sample/juan-reyes/todoapp-redux/src/Todos.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Todos = ({ todos }) => {
+const Todos = ({ todos, deleteTodo }) => {
 
   const todoList = todos.length ? (
     todos.map(todo => {
       return (
         <div className="collection-item" key={todo.id}>
-          <span>{todo.task}</span>
+          <span onClick={() => deleteTodo(todo.id)}>{todo.task}</span>
         </div>
       )
     })
