15cd35f5fc7fac0b0921c4557f96f8b5f48e37d5
JFBF <felichiki19@hotmail.com>
2019-05-15 21:25:22 -0500

    Commit: styles and initials functions

--- bogota-abril-2019/final_project/search-app/src/components/searchPanel/SearchPanel.js
+++ bogota-abril-2019/final_project/search-app/src/components/searchPanel/SearchPanel.js
@@ -1,34 +1,60 @@
 import React from 'react';
 import './SearchPanel.css';
 import fireBaseInit from '../FirebaseInit'
+import logo from '../../../src/logo.svg'
 
 const firebaseRef = fireBaseInit.database().ref().child("technology");
 class SearchPanel extends React.Component {
+  
   constructor(){
     super();
+    
     this.state = {
-      technologys : ["prueba1","prueba2"]
+      allTechnologys : [],
+      technologys : ["prueba1","prueba2"],
+      dataNotLoaded:true
+      
     }
+
+    this.findTechnologys = this.findTechnologys.bind(this);
   }
 
   componentDidMount(){
-    // read from firebase
+    // read from firebase 
     firebaseRef.once('value', snapshot => {
-        const technologys = snapshot.val().map((object)=>(object.title))
-        this.setState({technologys});
+       let allTechnologys  = this.allTechnologys = snapshot.val()
+        this.setState({technologys : allTechnologys.map((object) => (object.title)),
+          dataNotLoaded:false,allTechnologys});
       });
       
   }
+
+  findTechnologys(event){
+    var userInput = event.target.value.toLowerCase();
+    let results = this.allTechnologys.filter((element) =>{
+      return element.title.toLowerCase().includes(userInput)?element.title:"";
+    })
+    this.setState({technologys : results.map((element) => (element.title))});
+  }
+
+
 
   render () {
     
     return (
     <div className="SearchPanel">
       <header className="SearchPanel-header">
-        <p>
-        {this.state.technologys}
-        </p>
+        
+        <img src = {logo} alt = "logo"></img>
+        <input placeholder = "What are you looking for..." onKeyUp = {this.findTechnologys}
+              disabled = {this.state.dataNotLoaded}></input>
+        <img src = {logo} alt = "logo" ></img>
       </header>
+      <article>
+        <p>
+          {this.state.technologys}
+        </p>
+      </article>
     </div>
     )}
 }
