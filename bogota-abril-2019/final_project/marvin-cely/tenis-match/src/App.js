1f6a44b76c24c59a78dafe82a859f1baf08f2627
Marvin <mardancely@gmail.com>
2019-05-18 00:20:39 -0500

    Firebases integrated

--- bogota-abril-2019/final_project/marvin-cely/tenis-match/src/App.js
+++ bogota-abril-2019/final_project/marvin-cely/tenis-match/src/App.js
@@ -1,12 +1,59 @@
 import React from 'react';
 import './App.css';
+import firebase from 'firebase';
+import config from './services/config/firebaseConfig';
 
-function App() {
-  return (
-      <div>
-        Hello world!
-      </div>
-  );
+firebase.initializeApp(config);
+const dataBase = firebase.firestore();
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    // Crear Nuevo con ID automático
+    /* dataBase.collection("users").add({
+      first: "Holiiii",
+      last: "Helooo",
+      born: 1994
+    })
+    .then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch(function(error) {
+        console.error("Error adding document: ", error);
+    }); */
+
+    // Crear y actualizar
+    /* dataBase.collection("cities").doc("LA").set({
+      name: "Bogotá",
+      state: "Cundi",
+      country: "Colombia"
+    })
+    .then(function() {
+      console.log("Document successfully written!");
+    })
+    .catch(function(error) {
+      console.error("Error writing document: ", error);
+    }); */
+
+    /**
+     * add -> Nuevo con Id automático
+     * set -> Nuevo o actualización especificando todo 
+     * doc -> elemento con id automático pero vacio
+     * update-> Actualizar documento
+     * delete -> Eliminar documento
+     *  */
+  }
+
+
+  render() {
+    return (
+        <div>
+          Hello world!
+        </div>
+    );
+  }
 }
 
 export default App;
---
+++ bogota-abril-2019/final_project/marvin-cely/tenis-match/src/services/config/firebaseConfig.js
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: 'AIzaSyCEZkxAjwHd6D7xkigcyuojDfXfBWT8zz8',
+    authDomain: 'tenis-match-bootcamp.firebaseapp.com',
+    databaseURL: 'https://tenis-match-bootcamp.firebaseio.com',
+    projectId: 'tenis-match-bootcamp',
+    storageBucket: 'tenis-match-bootcamp.appspot.com',
+    messagingSenderId: '416982736252',
+    appId: '1:416982736252:web:5bd7f05f6d926e03'
+  };
+
+export default config;
