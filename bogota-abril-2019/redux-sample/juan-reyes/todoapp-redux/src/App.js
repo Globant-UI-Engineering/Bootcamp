253ab816aa91a5666cbd7edeb6e4a35182900500
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-05-19 17:09:26 -0500

    Advancing on ToDo app using Redux

--- bogota-abril-2019/redux-sample/juan-reyes/todoapp-redux/src/App.js
+++ bogota-abril-2019/redux-sample/juan-reyes/todoapp-redux/src/App.js
@@ -11,7 +11,13 @@ class App extends Component {
   }
 
   deleteTodo = (id) => {
-    console.log(id);
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    });
+    this.setState({
+      todos: todos
+    })
+
   }
 
   render() {
