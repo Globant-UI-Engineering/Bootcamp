c728ce4288308a3dbf8022d4b5ada34857a78930
quicazan <dfquicazanr@unal.edu.co>
2019-05-20 00:28:38 -0500

    Conecction to APIs working

--- bogota-abril-2019/final_project/daniel-quicazan/src/component/screen-component.js
+++ bogota-abril-2019/final_project/daniel-quicazan/src/component/screen-component.js
@@ -14,11 +14,14 @@ export class ScreenComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      showIntro: true,
       searchText: '',
-      currentPokemon: undefined
+      currentPokemon: undefined,
+      playDescription: false
     };
     this.handleSearchChange = this.handleSearchChange.bind(this);
     this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.handleSearchClick = this.handleSearchClick.bind(this);
   }
   
   componentWillMount() {
@@ -27,22 +30,48 @@ export class ScreenComponent extends Component {
   componentDidMount() {
   }
   
-  handleSearchChange($event) {
-    $event.preventDefault();
-    this.setState({searchText: $event.target.value.toLowerCase()})
+  intro() {
+    if (this.state.showIntro) {
+      return (
+        <Typist>
+          {this.welcomeSentece}
+          <Typist.Backspace count={this.welcomeSentece.length} delay={100} />
+          {this.letsBeginSentence}
+        </Typist>
+      )
+    }
   }
   
-  handleKeyPress(event) {
-    if (event.key === 'Enter') {
+  handleSearchChange($event) {
+    this.setState({playDescription: false});
+    this.setState({searchText: $event.target.value.toLowerCase()});
+  }
+  
+  handleKeyPress($event) {
+    if ($event.key === 'Enter') {
       this.search();
     }
+  }
+  
+  handleSearchClick($event) {
+    this.search();
   }
   
   search($event) {
     PokeapiService.search(this.state.searchText)
       .then((response) => {
         console.log(response.data);
-        this.setState({currentPokemon: response.data})
+        this.setState({
+          currentPokemon: response.data,
+          playDescription: true,
+          showIntro: false
+        }, () =>{
+          this.setState(
+            {
+              pokemonDescription: this.buildDescription()
+            }
+          )
+        });
       }).catch(error => {
       console.log(error)
     })
@@ -56,27 +85,82 @@ export class ScreenComponent extends Component {
     }
   }
   
+  buildDescription() {
+    let typesArray = this.state.currentPokemon.types;
+    let typesText = '';
+    if (typesArray.length === 1) {
+      typesText = typesArray[0].type.name
+    } else {
+      typesText = typesArray.reduce((a, b) => a.type.name + ' ' + b.type.name);
+    }
+    let abilitiesArray = this.state.currentPokemon.abilities;
+    let abilitiesText = abilitiesArray.map(x => x.ability.name).reduce((a, b) => a + ' and ' + b);
+    return this.state.searchText
+      + ', '
+      + typesText
+      + ' type pokemon, his abilities are '
+      + abilitiesText
+  }
+  
+  showDescription() {
+    if (this.state.pokemonDescription !== undefined) {
+      return (
+        <Typist>
+          <span className={'pokemon-description'}>
+          {
+            this.state.pokemonDescription
+          }
+          </span>
+        </Typist>
+      )
+    }
+  }
+  
+  playSpeech() {
+    if (this.state.playDescription) {
+      console.log(this.buildTextToSpeechUrl());
+      // return (
+      //   <div></div>
+      // );
+      return (
+        <audio autoPlay={true} src={this.buildTextToSpeechUrl()}/>
+      )
+    }
+  }
+  
+  buildTextToSpeechUrl() {
+    return 'http://api.voicerss.org/?key=ce496e662cbf48faa48dedcac67baba4&hl=en-us&c=mp3&src='
+      + this.state.pokemonDescription;
+  }
+  
   render() {
     return(
       <div className={'screen-container'}>
+        {
+          this.playSpeech()
+        }
         <div>
           <GlowingCircleComponent style={{margin: '2%'}} size='15px' darkColor={'#9e1c1b'} lightColor={'#e71f20'} shineColor={'#fbc9b7'}/>
           <GlowingCircleComponent style={{margin: '2%'}} size='15px' darkColor={'#9e1c1b'} lightColor={'#e71f20'} shineColor={'#fbc9b7'}/>
         </div>
         <div className={'screen-display'}>
-          <Typist>
-            {this.welcomeSentece}
-            <Typist.Backspace count={this.welcomeSentece.length} delay={100} />
-            {this.letsBeginSentence}
-          </Typist>
+          {
+            this.intro()
+          }
           <input className={'search-bar'}
                  type={'text'}
                  value={this.state.searchText}
                  onChange={this.handleSearchChange}
                  onKeyPress={this.handleKeyPress}
           />
+          <button onClick={this.handleSearchClick}>
+            Buscar
+          </button>
           {
             this.showSprite()
+          }
+          {
+            this.showDescription()
           }
         </div>
         <div className={'container-fluid'}>
