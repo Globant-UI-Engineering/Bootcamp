ce16e5e071a0d18e84b18d7b68b221913101f64a
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-05-20 15:51:39 -0500

    Adding react-router-dom and necessary components

---
+++ bogota-abril-2019/final_project/juan-reyes/surfers/client/src/components/surfers/SurferCreate.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SurferCreate = () => {
+  return <div>SurferCreate</div>
+};
+
+export default SurferCreate;
---
+++ bogota-abril-2019/final_project/juan-reyes/surfers/client/src/components/surfers/SurferList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SurferList = () => {
+  return <div>SurferList</div>
+};
+
+export default SurferList;
