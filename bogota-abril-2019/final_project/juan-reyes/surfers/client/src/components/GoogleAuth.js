3443b8bfa800105cea59b56bb4b080a65a28e48e
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-05-20 20:52:37 -0500

    Getting the onAuthChange on the fly

--- bogota-abril-2019/final_project/juan-reyes/surfers/client/src/components/GoogleAuth.js
+++ bogota-abril-2019/final_project/juan-reyes/surfers/client/src/components/GoogleAuth.js
@@ -10,10 +10,15 @@ class GoogleAuth extends React.Component {
         scope: 'email'
       }).then(() => {
         this.auth = window.gapi.auth2.getAuthInstance();
-        this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        this.auth.isSignedIn.listen(this.onAuthChange);
       });
     });
   }
+
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
 
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
