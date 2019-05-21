76795c808c8e730d1ee27d43c3b0dd9d2bde354b
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-05-21 16:11:04 -0500

    Using redux-forms and creating inputs for /surfers/new

--- bogota-abril-2019/final_project/juan-reyes/surfers/client/src/components/surfers/SurferCreate.js
+++ bogota-abril-2019/final_project/juan-reyes/surfers/client/src/components/surfers/SurferCreate.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const SurferCreate = () => {
-  return <div>SurferCreate</div>
+class SurferCreate extends React.Component {
+  renderInput({ input, label }) {
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />
+      </div>
+    )
+  }
+
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+        <Field name="title" component={this.renderInput} label="Title" />
+        <Field name="description" component={this.renderInput} label="Description" />
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
 };
 
-export default SurferCreate;
+export default reduxForm({
+  form: 'surferCreate'
+})(SurferCreate);
--- bogota-abril-2019/final_project/juan-reyes/surfers/client/src/reducers/authReducer.js
+++ bogota-abril-2019/final_project/juan-reyes/surfers/client/src/reducers/authReducer.js
@@ -1,6 +1,5 @@
 import { SIGN_IN, SIGN_OUT } from '../actions/types';
 
-
 const INITIAL_STATE = {
   isSignedIn: null,
   userId: null
