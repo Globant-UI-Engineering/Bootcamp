a1428f9dd2f9ee0dcb4987c63943ba7af9b8c2ea
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-05-20 20:44:33 -0500

    Authenticating with Google OAuth using email scope

---
+++ bogota-abril-2019/final_project/juan-reyes/surfers/client/src/components/GoogleAuth.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        clientId: '499369565828-vn86mctk722uu6uv835kvscbvf3p5gt6.apps.googleusercontent.com',
+        scope: 'email'
+      }).then(() => {
+        this.auth = window.gapi.auth2.getAuthInstance();
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+      });
+    });
+  }
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I don't know if we are signed in</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in!</div>
+    } else {
+      return <div>I am NOT signed in!</div>
+    }
+  }
+
+  render() {
+    return <div>{this.renderAuthButton()}</div>;
+  }
+}
+
+export default GoogleAuth;
