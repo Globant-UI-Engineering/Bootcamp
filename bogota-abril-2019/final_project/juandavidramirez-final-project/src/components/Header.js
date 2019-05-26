6564d91260cd78edda82e7053a03f3a2f63e6ac0
Juan David Ramirez Mendoza <juan.ramirez-me@mail.escuelaing.edu.co>
2019-05-26 10:02:01 -0500

    Footer icons styles added

--- bogota-abril-2019/final_project/juandavidramirez-final-project/src/components/Header.js
+++ bogota-abril-2019/final_project/juandavidramirez-final-project/src/components/Header.js
@@ -2,17 +2,21 @@ import React from "react";
 import { Link } from "@reach/router";
 import { AppBar, Typography, Toolbar, CssBaseline } from "@material-ui/core";
 
-const Header = props => (
-  <nav className="navigation-header-bar">
-    <Link to="/">
-      <h1>lol master</h1>
-    </Link>
-    <div className="section-links-container">
-      <Link to="/champions">champions</Link>
-      <Link to="/tierList">tier list</Link>
-      <Link to="/search">search</Link>
-    </div>
-  </nav>
-);
+class Header extends React.Component {
+  render() {
+    return (
+      <nav className="navigation-header-bar">
+        <Link to="/">
+          <h1>lol master</h1>
+        </Link>
+        <div className="section-links-container">
+          <Link to="/champions">champions</Link>
+          <Link to="/tierList">tier list</Link>
+          <Link to="/search">search</Link>
+        </div>
+      </nav>
+    );
+  }
+}
 
 export default Header;
