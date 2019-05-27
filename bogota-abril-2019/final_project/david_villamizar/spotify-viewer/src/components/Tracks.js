db736dd2933db732d817eaa0839d24cef43b1874
David Villamizar <davl3232@gmail.com>
2019-05-26 20:19:35 -0500

    Added preview on hover or tap hold.

--- bogota-abril-2019/final_project/david_villamizar/spotify-viewer/src/components/Tracks.js
+++ bogota-abril-2019/final_project/david_villamizar/spotify-viewer/src/components/Tracks.js
@@ -1,9 +1,33 @@
 import React from "react";
 import styles from "./Tracks.module.css";
 
-function Track({ name, id, album, popularity }) {
+let currentAudio;
+
+function playAudio(url) {
+  if (currentAudio) {
+    currentAudio.pause();
+  }
+  currentAudio = new Audio(url);
+  currentAudio.play();
+}
+
+function stopAudio() {
+  if (currentAudio) {
+    currentAudio.pause();
+    currentAudio = undefined;
+  }
+}
+
+function Track({ name, id, album, popularity, preview_url }) {
   return (
-    <li key={id} className={styles.track}>
+    <li
+      key={id}
+      className={styles.track}
+      onMouseEnter={e => playAudio(preview_url)}
+      onMouseLeave={e => stopAudio()}
+      onTouchStart={e => playAudio(preview_url)}
+      onTouchEnd={e => stopAudio()}
+    >
       <h3 title={name}>{name}</h3>
       <h4 title={album.name}>{album.name}</h4>
       {popularity ? <p>{popularity}</p> : null}
