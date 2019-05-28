f0e5ccc5fce3d71e32cbeb3ee50076cabc57555b
Laura Brugés <laura.bruges@globant.com>
2019-05-28 17:27:30 -0500

    Adding styles to player component pending progress bar

--- bogota-abril-2019/final_project/laura_bruges-Final_project/spotify-react-project/src/components/Player.jsx
+++ bogota-abril-2019/final_project/laura_bruges-Final_project/spotify-react-project/src/components/Player.jsx
@@ -1,19 +1,45 @@
 import React from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import '../styles/Player.css';
+import { play } from 'react-icons-kit/fa/play';
+import { backward } from 'react-icons-kit/fa/backward';
+import { forward } from 'react-icons-kit/fa/forward';
+import { pause } from 'react-icons-kit/fa/pause';
+import Icon from 'react-icons-kit';
+
 class Player extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            playing: false
+        }
+        this.handlePlayClick = this.handlePlayClick.bind(this);
+    }
+
+    handlePlayClick(){
+        let nowPlaying = this.state.playing;
+        this.setState({
+            playing: !nowPlaying
+        })
+    }
+
     render(){
         return (
             <footer className='page-footer fixed-bottom'>
                 <Container>
+                    <Row>
+                        <Col md={12} className='centered'>
+                            <p><span className='song-title'>Song title</span> - <span>Artist</span></p>
+                        </Col>
+                    </Row>
                     <Row>         
-                        <Col md={12}>
-                            <input type='range'/>
+                        <Col md={9} className='centered'>
+                            <input style={{width: '100%'}} type='range'/>
                         </Col>
                     </Row>
                     
                     <Row>
-                        <Col md={12}>
+                        <Col md={9} className='centered'>
                             <span className='float-left'>00:00</span>
                             <span className='float-right'>03:58</span>
                         </Col>
@@ -21,9 +47,9 @@ class Player extends React.Component {
 
                     <Row>
                         <Col md={12}>
-                            <Button>Prev</Button>
-                            <Button>Play</Button>
-                            <Button>Next</Button>
+                            <Button className='player-control change-track-btn'><Icon icon={backward}></Icon></Button>
+                            <Button className='player-control play-btn' onClick={this.handlePlayClick}> {this.state.playing ? <Icon icon={pause}></Icon> : <Icon icon={play}></Icon>} </Button>
+                            <Button className='player-control change-track-btn'><Icon icon={forward}></Icon></Button>
                         </Col>
                     </Row>
                 </Container>
