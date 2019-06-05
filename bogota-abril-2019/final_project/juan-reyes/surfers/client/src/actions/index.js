011e4a4e880eaf7a70c0d94496effaebb8ccfc60
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-06-05 12:45:34 -0300

    Configuring the REST route inside the action creator | fetchSurfers, fetchSurfer, editSurfer and deleteSurfer

--- bogota-abril-2019/final_project/juan-reyes/surfers/client/src/actions/index.js
+++ bogota-abril-2019/final_project/juan-reyes/surfers/client/src/actions/index.js
@@ -1,5 +1,13 @@
 import surfers from '../apis/surfers';
-import { SIGN_IN, SIGN_OUT } from './types';
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_SURFER,
+  FETCH_SURFERS,
+  FETCH_SURFER,
+  DELETE_SURFER,
+  EDIT_SURFER,
+} from './types';
 
 export const signIn = (userId) => {
   return {
@@ -16,5 +24,31 @@ export const signOut = () => {
 
 // I am using an async action creator thanks to Redux Thunk
 export const createSurfer = formValues => async dispatch => {
-  surfers.post('/surfers', formValues);
+  const response = await surfers.post('/surfers', formValues);
+
+  dispatch({ type: CREATE_SURFER, payload: response.data });
+};
+
+export const fetchSurfers = () => async dispatch => {
+  const response = await surfers.get('/surfers');
+
+  dispatch({ type: FETCH_SURFERS, payload: response.data });
+};
+
+export const fetchSurfer = (id) => async dispatch => {
+  const response = await surfers.get(`/surfers/${id}`);
+
+  dispatch({ type: FETCH_SURFER, payload: response.data });
+};
+
+export const editSurfer = (id, formValues) => async dispatch => {
+  const response = await surfers.put(`/surfers/${id}`, formValues);
+
+  dispatch({ type: EDIT_SURFER, payload: response.data });
+};
+
+export const deleteSurfer = (id) => async dispatch => {
+  await surfers.delete(`/surfers/${id}`);
+
+  dispatch({ type: DELETE_SURFER, payload: id });
 };
